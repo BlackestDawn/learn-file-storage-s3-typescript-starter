8bc171ce3b3e551d9ab1818d3fa7d757dc94360c
Alexander Stauch <blacke4dawn@gmail.com>
2025-07-08 19:59:18 +0200

    chapter 1: file storage

--- src/api/thumbnails.ts
+++ src/api/thumbnails.ts
@@ -1,40 +1,11 @@
 import { getBearerToken, validateJWT } from "../auth";
 import { respondWithJSON } from "./json";
-import { getVideo } from "../db/videos";
+import { getVideo, updateVideo } from "../db/videos";
 import type { ApiConfig } from "../config";
 import type { BunRequest } from "bun";
-import { BadRequestError, NotFoundError } from "./errors";
+import { BadRequestError, NotFoundError, UserForbiddenError } from "./errors";
+import path from "path";
 
-type Thumbnail = {
-  data: ArrayBuffer;
-  mediaType: string;
-};
-
-const videoThumbnails: Map<string, Thumbnail> = new Map();
-
-export async function handlerGetThumbnail(cfg: ApiConfig, req: BunRequest) {
-  const { videoId } = req.params as { videoId?: string };
-  if (!videoId) {
-    throw new BadRequestError("Invalid video ID");
-  }
-
-  const video = getVideo(cfg.db, videoId);
-  if (!video) {
-    throw new NotFoundError("Couldn't find video");
-  }
-
-  const thumbnail = videoThumbnails.get(videoId);
-  if (!thumbnail) {
-    throw new NotFoundError("Thumbnail not found");
-  }
-
-  return new Response(thumbnail.data, {
-    headers: {
-      "Content-Type": thumbnail.mediaType,
-      "Cache-Control": "no-store",
-    },
-  });
-}
 
 export async function handlerUploadThumbnail(cfg: ApiConfig, req: BunRequest) {
   const { videoId } = req.params as { videoId?: string };
@@ -48,6 +19,35 @@ export async function handlerUploadThumbnail(cfg: ApiConfig, req: BunRequest) {
   console.log("uploading thumbnail for video", videoId, "by user", userID);
 
   // TODO: implement the upload here
+  const formData = await req.formData();
+  const file = formData.get("thumbnail");
 
-  return respondWithJSON(200, null);
+  if (!(file instanceof File)) {
+    throw new BadRequestError("Invalid file");
+  }
+
+  const MAX_UPLOAD_SIZE = 10 << 20; // 10 MB
+  if (file.size > MAX_UPLOAD_SIZE) {
+    throw new BadRequestError("File too large");
+  }
+
+  const mediaType = file.type;
+  const data = await file.arrayBuffer();
+  const fileName = videoId + path.extname(file.name);
+  const filePath = path.join(cfg.assetsRoot, fileName);
+
+  await Bun.write(filePath, new Uint8Array(data));
+
+  const videoData = getVideo(cfg.db, videoId);
+  if (videoData?.userID !== userID) {
+    throw new UserForbiddenError("Not authorized to upload thumbnail");
+  }
+
+  const thumbnailURL = `http://localhost:${cfg.port}/assets/${fileName}`
+
+  videoData.thumbnailURL = thumbnailURL;
+
+  updateVideo(cfg.db, videoData);
+
+  return respondWithJSON(200, videoData);
 }
